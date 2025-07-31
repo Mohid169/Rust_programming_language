cf26f7b0ff4aa419527f7e3ec4d91ca0e1365c32
Mohid <mohidkhan169@gmail.com>
2025-07-30 18:05:41 -0700

    fixing let varible

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,8 +3,8 @@ use rand::Rng;
 
 fn main() {
     println!("Guess The Number!");
-    
-    secret_number = rand::thread_rng().gen_range(1..=100);
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number: {secret_number}");
     println!("Please Input your guess");
